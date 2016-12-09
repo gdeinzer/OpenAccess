761f530097c23262fcd9093b307e55901a688a2a
Gernot Deinzer <g.deinzer@gmail.com>
2016-12-09 16:49:53 +0100

    ubtstituted links with real files

--- lib/plugins/EPrints/Plugin/Stats/Processor/EPrint/PaidArticles.pm
+++ lib/plugins/EPrints/Plugin/Stats/Processor/EPrint/PaidArticles.pm
@@ -1,1 +1,59 @@
-/usr/share/eprints3/archives/oatest/cfg/plugins/EPrints/Stats/Process/PaidArticles.pm
+package EPrints::Plugin::Stats::Processor::EPrint::PaidArticles;
+
+our @ISA = qw/ EPrints::Plugin::Stats::Processor /;
+
+use strict;
+
+# Processor::EPrint::PaidArticles
+#
+# Processed the number of paid Articles over time. Provides the 'eprint_deposits' datatype.
+# 
+
+sub new
+{
+        my( $class, %params ) = @_;
+	my $self = $class->SUPER::new( %params );
+
+	$self->{provides} = [ "paid_articles" ];
+
+	$self->{disable} = 0;
+
+	return $self;
+}
+
+sub process_record
+{
+	my ($self, $eprint ) = @_;
+
+	my $epid = $eprint->get_id;
+	return unless( defined $epid );
+
+	my $status = $eprint->get_value( "eprint_status" );
+	unless( defined $status ) 
+	{
+##		print STDERR "\nstatus not set for eprint=".$eprint->get_id;
+		return;
+	}
+
+        my $cost = $eprint->get_value( "oa_total_cost" );
+ 	unless( defined $cost and $cost>0 ) 
+	{
+##		print STDERR "\nstatus not set for eprint=".$eprint->get_id;
+		return;
+	}
+
+
+
+	my $datestamp = $eprint->get_value( "date" ) || $eprint->get_value( "datestamp" ) || $eprint->get_value( "lastmod" );
+
+	my $date = $self->parse_datestamp( $self->{session}, $datestamp );
+
+	my $year = $date->{year};
+	my $month = $date->{month};
+	my $day = $date->{day};
+
+	$self->{cache}->{"$year$month$day"}->{$epid}->{$status}++;
+}
+
+
+1;
--- lib/plugins/EPrints/Plugin/Stats/Processor/EPrint/TotalCosts.pm
+++ lib/plugins/EPrints/Plugin/Stats/Processor/EPrint/TotalCosts.pm
@@ -1,1 +1,56 @@
-/usr/share/eprints3/archives/oatest/cfg/plugins/EPrints/Stats/Process/EPrint/TotalCosts.pm
+package EPrints::Plugin::Stats::Processor::EPrint::TotalCosts;
+
+our @ISA = qw/ EPrints::Plugin::Stats::Processor /;
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+	my $self = $class->SUPER::new( %params );
+
+#  provide the name of the datatype
+	$self->{provides} = [ "oa_total_cost" ];
+
+	$self->{disable} = 0;
+
+	return $self;
+}
+
+sub process_record
+{
+	my ($self, $eprint ) = @_;
+
+	my $epid = $eprint->get_id;
+	return unless( defined $epid );
+
+	my $status = $eprint->get_value( "eprint_status" );
+	unless( defined $status ) 
+	{
+##		print STDERR "IRStats2: warning - status not set for eprint=".$eprint->get_id."\n";
+		return;
+	}
+
+	return unless( $status eq 'archive' );
+
+	my $datestamp = $eprint->get_value( "date" ) || $eprint->get_value( "datestamp" ) || $eprint->get_value( "lastmod" );
+	my $date = $self->parse_datestamp( $self->{session}, $datestamp );
+
+	my $year = $date->{year};
+	my $month = $date->{month};
+	my $day = $date->{day};
+
+# get the cost count
+	my $total_cost_count = $eprint->get_value( "oa_total_cost" );
+#
+# store the cost per eprint id
+	if (defined $total_cost_count)
+	{
+		$self->{cache}->{"$year$month$day"}->{$epid}->{"oa_total_cost"} = $total_cost_count;
+	}
+}
+
+1;
+
+	
+
